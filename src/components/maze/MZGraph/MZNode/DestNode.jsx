ab1d709bb2d052c87333e31b0886f47c0ac0d84e
Nate Marder <ntmarder@gmail.com>
2019-07-18 17:45:45 -0700

    cleans up destnode

--- src/components/maze/MZGraph/MZNode/DestNode.jsx
+++ src/components/maze/MZGraph/MZNode/DestNode.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 
-const DestNode = props => <circle
-  className="mz-node dest-node"
-  cx={props.x}
-  cy={props.y}
-  r={props.r} 
-/>
+const DestNode = props => (
+  <circle
+    className="mz-node dest-node"
+    cx={props.x}
+    cy={props.y}
+    r={props.r}
+  />
+);
 
 export default DestNode;
